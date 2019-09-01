7887121ade558709581a035fd41b85cd2beff706
Kristi Hwang <khwang@sbgtv.com>
2019-09-01 13:02:58 -0700

    make call to api

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import Axios from 'axios';
+
+const KEY = 'AIzaSyBLhDxA6K3TUOKZei0xOZBEdu9plqEXwT8';
+
+
+export default Axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+    }
+});
