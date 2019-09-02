8b76edad2b1f020b009bb93ee1afd3b7884ecb38
Kristi Hwang <khwang@sbgtv.com>
2019-09-02 11:13:40 -0700

    update video player with new search

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,20 @@ class App extends React.Component {
     selectedVideo: null
   };
 
+  componentDidMount(){
+    this.onTermSubmit('up');
+  }
+
   onTermSubmit = async term => {
     const resp = await youtube.get('/search', {
       params: {
         q: term
       }
     });
-    this.setState({ videos: resp.data.items });
+    this.setState({
+      videos: resp.data.items,
+      selectedVideo:resp.data.items[0]
+    });
   };
 
   onVideoSelect = (video) => {
@@ -27,8 +34,19 @@ class App extends React.Component {
    return (
      <div className="ui container">
       <SearchBar onTermSubmit={this.onTermSubmit} />
-      <VideoDetail video={this.state.selectedVideo} />
-      <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={this.state.selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList
+              onVideoSelect={this.onVideoSelect}
+              videos={this.state.videos}
+            />
+          </div>
+        </div>
+      </div>
      </div>
    );
   }
